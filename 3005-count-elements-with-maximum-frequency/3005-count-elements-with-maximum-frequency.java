6a293d10d176263a9e37368cf992aadda5c4ed8c
sanskriti666 <sanskriti.gla1_cs25@gla.ac.in>
2026-09-13 22:29:30 +0530

    Time: 1 ms (99.54%), Space: 44.2 MB (17.06%) - LeetHub

---
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        int[] freq = new int[101];
+        for (int num : nums) {
+            freq[num]++;
+        }
+        int maxFreq = 0;
+       for (int i = 0; i < freq.length; i++) {
+            maxFreq = Math.max(maxFreq, freq[i]);
+        }
+        int ans = 0;
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] == maxFreq) {
+                ans += freq[i];
+            }
+        }
+        return ans;
+    }
+}
